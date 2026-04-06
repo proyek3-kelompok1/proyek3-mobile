ff9012d66c5cc4474cfd46400ce50b2697c9ea0d
unknown <karinasiti2101@gmail.com>
2026-04-06 13:57:45 +0700

    karin

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.1.4:8000/api";
+  static const String baseUrl = "http://192.168.1.7:8000/api";
   static const String education = "$baseUrl/education";
   static const String services = "$baseUrl/services";
   static const String booking = "$baseUrl/bookings";
