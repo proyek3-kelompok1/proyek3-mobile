e1a18e6d3081b36c156d7319ec7a29ed3b0b1e5f
unknown <karinasiti2101@gmail.com>
2026-05-03 06:16:53 +0700

    03

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = "http://10.0.2.2:8000/api";
+  static const String baseUrl = "https://ipzonex.my.id/api";
+  static const String storageUrl = "https://ipzonex.my.id/storage";
   static const String education = "$baseUrl/education";
   static const String services = "$baseUrl/services";
   static const String booking = "$baseUrl/bookings";
